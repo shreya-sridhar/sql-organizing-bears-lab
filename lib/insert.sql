60514b6d043f4a93ddcd3fd5b75ece967341ccbf
shreya-sridhar <shreyasridhar1109@gmail.com>
2020-11-30 19:11:14 +0000

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament, alive) values ( 'Mr.Chocolate',5,'M','black','cool',TRUE);
+INSERT INTO bears (name,age,sex,color,temperament, alive) values ( 'Rowdy',5,'M','black','cool',TRUE);
+INSERT INTO bears (name,age,sex,color,temperament, alive) values ('Tabitha',5,'F','black','cool',TRUE);
+INSERT INTO bears (name,age,sex,color,temperament, alive) values ('Sergeant Brown',5,'M','black','cool',TRUE);
+INSERT INTO bears (name,age,sex,color,temperament, alive) values ( 'Melissa',5,'F','black','goofy',TRUE);
+INSERT INTO bears (name,age,sex,color,temperament, alive) values ('Grinch',5,'M','black','goofy',TRUE);
+INSERT INTO bears (name,age,sex,color,temperament, alive) values ( 'Wendy',5,'F','black','cool',TRUE);
+INSERT INTO bears (name,age,sex,color,temperament, alive) values ( NULL ,5,'F','black','cool',TRUE);
